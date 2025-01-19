dff63613b2e19715527e8bac9465d49c0b6a0c9d
txnujk09 <tanujkakumani2@gmail.com>
2025-01-19 16:40:44 +0000

    Update monte_carlo.py
    - geometric brownian motion function

--- monte_carlo/monte_carlo.py
+++ monte_carlo/monte_carlo.py
@@ -1,1 +1,10 @@
+import numpy as np
 
+def geometric_brownian_motion(IS, ER, sigma, T, timesteps, simulations):
+    dt = T / timesteps
+    random_factors = np.random.normal(0, 1, (simulations, timesteps))
+    paths = np.zeros((simulations, timesteps))
+    paths[:, 0] = IS
+    for t in range(1, timesteps):
+        paths[:, t] = paths[:, t-1] * np.exp((ER - 0.5 * sigma**2) * dt + sigma * np.sqrt(dt) * random_factors[:, t-1])
+    return paths
