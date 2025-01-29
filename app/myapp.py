3258dd0c0b00d6dac58fe164c93ee75a751e4d86
txnujk09 <tanujkakumani2@gmail.com>
2025-01-29 19:08:31 +0000

    fixing the db_create.all error

--- app/myapp.py
+++ app/myapp.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, current_app
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from models import db
@@ -10,15 +10,18 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object('config.Config')  # Load app configurations
 
-    #creating database tables
-    db.create_all()
-    
     # Initialize extensions
     db.init_app(app)
     migrate.init_app(app, db)
+
+    #creating database tables
+    with app.app_context():
+     print("Current app:", current_app.name)
+     db.create_all()
 
     # Register Blueprints
     from app.routes import routes
     app.register_blueprint(routes)
 
     return app
+
