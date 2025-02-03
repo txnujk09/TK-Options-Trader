f6ec84f2483eded289eae2e878ad3a792fd1e2b3
txnujk09 <tanujkakumani2@gmail.com>
2025-02-03 19:09:45 +0000

    created and initalised database

---
+++ setup_db.py
@@ -0,0 +1,20 @@
+#creating and intialising database
+
+# Adding project root to sys.path for module resolution
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), './app')))
+print(sys.path)
+
+from app.myapp import create_app #Import create_app function from app 
+from app.models import db  #Import the database instance
+
+# Create the Flask app context
+app = create_app()
+
+with app.app_context():
+    db.init_app(app)
+    db.create_all()
+    print("Database created successfully!")
