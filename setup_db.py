8289db36fc435ec5678e638bea0654851773ee26
txnujk09 <tanujkakumani2@gmail.com>
2025-02-03 19:12:25 +0000

    fixed the error with creating and initialising the database

--- setup_db.py
+++ setup_db.py
@@ -15,6 +15,4 @@ from app.models import db  #Import the database instance
 app = create_app()
 
 with app.app_context():
-    db.init_app(app)
-    db.create_all()
     print("Database created successfully!")
